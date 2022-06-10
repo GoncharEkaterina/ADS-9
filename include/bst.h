4259c96711d81a2f8405dfd7e423b2873d8d73cc
GoncharenkoK <99185069+GoncharEkaterina@users.noreply.github.com>
2022-06-10 15:49:25 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,3 +3,31 @@
 #define INCLUDE_BST_H_
 
 #endif  // INCLUDE_BST_H_
+
+template <typename T>
+class BST {
+  public: 
+    struct Node {
+      T value;
+      int count;
+      Node *left;
+      Node *right;
+    };
+  private:
+    Node* root;
+    Node* addNode(Node *, T);
+    void printTree(Node*);
+    int depthTree(Node*);
+    int searchNode(Node*, T);
+    void delTree(Node*);
+    Node* delNode(Node*,int);
+  public:
+  BST();
+  ~BST();
+  void add(T);
+  void print();
+  int depth();
+  int search(T);
+  void clear();
+  void remove(int);
+};
